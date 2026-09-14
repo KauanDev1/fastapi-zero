3723b91de7f4b6d2585c044782ec1b98300be295
KauanDev1 <kauanturcato@gmail.com>
2026-09-14 17:49:57 -0400

    iniciando crud

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-app = FastAPI()
+
+from fastapi import FastAPI
+from fastapi_zero.schemas import UserSchema
+
+
+app = FastAPI(title='API que funfa!')
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá mundo!'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserSchema)
+def create_user(user: UserSchema):
+    return user
+
+
+
+
