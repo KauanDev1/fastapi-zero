e94b19d71d37089b5008298a6d4b65b0df2fdcd7
KauanDev1 <kauanturcato@gmail.com>
2026-09-21 17:34:51 -0400

    feat(put): adicionado rota put e melhorado anotações

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -21,12 +21,12 @@ def read_root():
 # Metodo de criação como POST precisam receber uma variavel q guardar o valor
 # recebido e de um schema(schema é como se fosse um contrato q pré estabelece
 # oq precisa ser recebido de dados)
-# User é a variavel que armazena os dados recebidos do metodo http, e o 
+# User é a variavel que armazena os dados recebidos do metodo http, e o
 # UserSchema é como os dados devem ser armazenado.
 def create_user(user: UserSchema):
     # user_with_id é uma variavel q cria o usuario com id para adicionar-lo ao
-    # database, ele usa a função UserDB que é o contrato de input o userschema 
-    # com id, dai ele descompacta com a função **model_dump() q fica igual o 
+    # database, ele usa a função UserDB que é o contrato de input o userschema
+    # com id, dai ele descompacta com a função **model_dump() q fica igual o
     # trem ali, e add o user q veio do input e poe só um id nele.
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     # Model dump faz o trabalho braçal de:         username=user.username,
@@ -39,14 +39,21 @@ def create_user(user: UserSchema):
     return user_with_id
 
 
+# Pydantic espera receber um dicionário com a chave chamada 'users' ( o
+# schema de resposta é o UserList onde retorna o usuarios como uma lista de
+# User) para poder validar e retornar o metdo get
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users():
     return {'users': database}
 
-@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+
+# {user_id} é um input para selecionar o usuario q será atualizado, logo para
+# ele ser usado na função ele deve ser adicionado dentro do parametros da
+# função
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
 def update_user(user_id: int, user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=user_id)
-
     database[user_id - 1] = user_with_id
-
     return user_with_id
