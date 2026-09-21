c83888cd4084689248a22bed37a8c83165c70bb3
KauanDev1 <kauanturcato@gmail.com>
2026-09-21 17:16:56 -0400

    feat(put): adicionando rota put e melhorando anotações

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -21,15 +21,20 @@ def read_root():
 # Metodo de criação como POST precisam receber uma variavel q guardar o valor
 # recebido e de um schema(schema é como se fosse um contrato q pré estabelece
 # oq precisa ser recebido de dados)
+# User é a variavel que armazena os dados recebidos do metodo http, e o 
+# UserSchema é como os dados devem ser armazenado.
 def create_user(user: UserSchema):
     # user_with_id é uma variavel q cria o usuario com id para adicionar-lo ao
-    # database
+    # database, ele usa a função UserDB que é o contrato de input o userschema 
+    # com id, dai ele descompacta com a função **model_dump() q fica igual o 
+    # trem ali, e add o user q veio do input e poe só um id nele.
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     # Model dump faz o trabalho braçal de:         username=user.username,
-    #                                             email=user.email,
-    #                                             password=user.password,
+    #                                              email=user.email,
+    #                                              password=user.password,
     # pro dev nao precisar escrever na mão.
 
+    # add o usuario no banco de dados ficticio.
     database.append(user_with_id)
     return user_with_id
 
@@ -37,3 +42,11 @@ def create_user(user: UserSchema):
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users():
     return {'users': database}
+
+@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
